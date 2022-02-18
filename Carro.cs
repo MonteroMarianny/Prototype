d610cc4575605d28b03bbbcef9e938d417e13f0f
Marianny <Marianny@LAPTOP-K0014UM0>
2022-02-17 23:13:48 -0400

    Agregar archivos de proyecto.

---
+++ Carro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Carro : ICloneable
+    {
+        public string Marca { get; set; }
+        public int Año { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+     class Prototype
+    {
+        static void Main(string[]args)
+        {
+            Carro oCarro = new Carro() { Marca = "Audi", Año = 2021 };
+            Carro oCarro2 = oCarro;
+            oCarro2.Año = 2022;
+
+            Console.WriteLine(oCarro.Marca);
+            Console.WriteLine(oCarro.Año);
+            Console.WriteLine(oCarro2.Marca);
+            Console.WriteLine(oCarro2.Año);
+        }
+    }
+}
